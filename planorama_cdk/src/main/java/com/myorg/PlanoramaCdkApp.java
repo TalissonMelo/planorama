1b8cd6ac4af1bb8bd4e293fa267570d192a263d7
Talisson Melo <talis.melo@petize.com.br>
2024-11-07 07:32:46 -0300

    Created Stack ECR

--- planorama_cdk/src/main/java/com/myorg/PlanoramaCdkApp.java
+++ planorama_cdk/src/main/java/com/myorg/PlanoramaCdkApp.java
@@ -4,13 +4,29 @@ import software.amazon.awscdk.App;
 import software.amazon.awscdk.Environment;
 import software.amazon.awscdk.StackProps;
 
-import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class PlanoramaCdkApp {
     public static void main(final String[] args) {
         App app = new App();
 
+        Environment environment = Environment.builder()
+                .account("ACCOUNT_NUMBER")
+                .region("REGION")
+                .build();
 
+        Map<String, String> infraTags = new HashMap<>();
+        infraTags.put("team", "Planorama");
+        infraTags.put("cost", "DevelopmentAndDevOps");
+
+        EcrStack ecrStack = new EcrStack(app, "ECR", StackProps.builder()
+                .env(environment)
+                .tags(infraTags)
+                .build());
+
+
+        //ecrStack.getPlanoramaBackendRepository();
 
         app.synth();
     }
